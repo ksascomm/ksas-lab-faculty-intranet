c75937ce2350a3570b68dd8bf4d421bdb4a18bda
ksascomm <ksasweb@jhu.edu>
2017-04-11 09:45:56 -0400

    lightbox2 swapped out with fancybox3

--- assets/js/scripts/wp-foundation.js
+++ assets/js/scripts/wp-foundation.js
@@ -6,12 +6,12 @@ and Foundation play nice together.
 jQuery(document).ready(function() {
 
     // Remove empty P tags created by WP inside of Accordion and Orbit
-    jQuery('.accordion p:empty, .orbit p:empty').remove();
+  jQuery('.accordion p:empty, .orbit p:empty').remove();
 
 	 // Makes sure last grid item floats left
 	jQuery('.archive-grid .columns').last().addClass( 'end' );
 
-	// Adds Flex Video to YouTube and Vimeo Embeds
+	 // Adds Flex Video to YouTube and Vimeo Embeds
   jQuery('iframe[src*="youtube.com"], iframe[src*="vimeo.com"]').each(function() {
     if ( jQuery(this).innerWidth() / jQuery(this).innerHeight() > 1.5 ) {
       jQuery(this).wrap("<div class='widescreen flex-video'/>");
@@ -19,5 +19,9 @@ jQuery(document).ready(function() {
       jQuery(this).wrap("<div class='flex-video'/>");
     }
   });
+
+  // Adds Fancybox to images and image gallerys
+  jQuery('a[href*=".jpg"], a[href*=".jpeg"], a[href*=".png"], a[href*=".gif"]').fancybox(); 
+  jQuery('.gallery a').attr("data-fancybox", "group");
 
 });
