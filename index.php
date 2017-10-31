6235a1902a226ad674ef2de0c14df19d9462748b
ksascomm <ksasweb@jhu.edu>
2017-10-31 15:48:30 -0400

    add featured image to news archive page

--- index.php
+++ index.php
@@ -10,10 +10,23 @@ $theme_option = flagship_sub_get_global_options();
 
 		    <main id="main" class="medium-9 medium-push-3 columns" role="main">
 
-		    	<h1 class="page-title"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
-		
-			    <?php
-                if (have_posts() ) : while (have_posts() ) : the_post(); ?>
+				<?php
+				// If a featured image is set, insert into layout and use Interchange
+				// to select the optimal image size per named media query.
+				if ( has_post_thumbnail( $post->ID ) ) :
+			    ?>
+					<header class="featured-hero" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
+					</header>
+
+					<h1 class="entry-title clear"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
+				
+				<?php else : ?>	
+
+					<h1 class="page-title"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
+					
+				<?php endif; ?>
+
+			    <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
 						<div class="news-feed">
 							<!-- To see additional archive styles, visit the /parts directory -->
 							<?php get_template_part( 'parts/loop', 'archive' ); ?>
