549c652714508b0f4e51942b29fad7cf568eee27
ksascomm <ksasweb@jhu.edu>
2018-02-06 14:51:26 -0500

    order role terms by slug, not ID

--- template-people-directory.php
+++ template-people-directory.php
@@ -8,7 +8,7 @@ get_header();
 	$theme_option = flagship_sub_get_global_options();
 	$roles = get_terms(
         'role', array(
-			'orderby'       => 'ID',
+			'orderby'       => 'slug',
 			'order'         => 'ASC',
 			'hide_empty'    => true,
 		)
