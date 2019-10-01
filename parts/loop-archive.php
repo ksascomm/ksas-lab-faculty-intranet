bc03963e7bd9f55ac66d95125c26b327e96a4afc
ksascomm <ksasweb@jhu.edu>
2019-10-01 11:59:38 -0400

    add missing ID

--- parts/loop-archive.php
+++ parts/loop-archive.php
@@ -1,7 +1,7 @@
 <article <?php post_class(''); ?> itemscope itemtype="http://schema.org/BlogPosting" aria-labelledby="post-<?php the_ID(); ?>">
 	<header class="article-header" aria-label="<?php the_title(); ?>">	
 		<h1 class="entry-title single-title" itemprop="headline">
-			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>"><?php the_title(); ?></a>
+			<a href="<?php the_permalink(); ?>" rel="bookmark" title="<?php the_title_attribute(); ?>" id="post-<?php the_ID(); ?>"><?php the_title(); ?></a>
 		</h1>
 		<?php get_template_part( 'parts/content', 'byline' ); ?>
 	</header> <!-- end article header -->
