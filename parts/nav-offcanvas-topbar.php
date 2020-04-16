4e5f5cd44f4810245d6024e99b95a32fe7ebdd23
ksascomm <ksascomm@gmail.com>
2020-04-16 11:42:15 -0400

    refactor header shield and text code

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -13,21 +13,28 @@
 </div>
 
 <div class="top-bar hide-for-print" id="top-bar-menu">
-	<div class="top-bar-left float-left">
-		<ul class="menu show-for-medium">
-			<li><a href="http://krieger.jhu.edu">
-				<?php $theme_option = flagship_sub_get_global_options();
-				$color_scheme = $theme_option['flagship_sub_color_scheme'];
-				if ('spirit' === $color_scheme ) : ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-black.png" class="float-center" alt="Krieger School of Arts and Sciences">
-				<?php else: ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-md.png" alt="Krieger School of Arts and Sciences">
-				<?php endif; ?>	
-			</a></li>
-			<li><h1><a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'title' ); ?><br>
-				<small><?php echo get_bloginfo( 'description' ); ?></small></a></h1>
-			</li>
-		</ul>
+	
+		<div class="show-for-medium">
+			<div class="grid-x">
+				<div class="nav-shield">
+					<a href="http://krieger.jhu.edu">
+					<?php $theme_option = flagship_sub_get_global_options();
+					$color_scheme = $theme_option['flagship_sub_color_scheme'];
+					if ('spirit' === $color_scheme ) : ?>
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-black.png" class="float-center" alt="Krieger School of Arts and Sciences">
+					<?php else: ?>
+						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-md.png" alt="Krieger School of Arts and Sciences">
+					<?php endif; ?>	
+					</a>
+				</div>
+				<div class="site-title">
+					<h1>
+						<a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'title' ); ?><br>
+						<small><?php echo get_bloginfo( 'description' ); ?></small></a>
+					</h1>
+				</div>
+			</div>
+		</div>
 		<ul class="vertical menu show-for-small-only">
 			<li>
 
@@ -42,7 +49,6 @@
 				<small><?php echo get_bloginfo( 'description' ); ?></small></a></h1>
 			</li>
 		</ul>
-	</div>
 	<div class="top-bar-right float-right show-for-small-only">
 		<ul class="menu">
 			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
