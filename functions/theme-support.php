94a575675bac3349657d36065df1516b54cfbee2
ksascomm <ksascomm@gmail.com>
2020-04-30 16:04:25 -0400

    Refactor favicon function

--- functions/theme-support.php
+++ functions/theme-support.php
@@ -50,3 +50,21 @@ function joints_theme_support() {
 } /* end theme support */
 
 add_action( 'after_setup_theme', 'joints_theme_support' );
+
+add_action( 'wp_head',    'ksas_default_site_icon', 99 );
+add_action( 'login_head', 'ksas_default_site_icon', 99 );
+
+function ksas_default_site_icon()
+{
+    if( ! has_site_icon()  && ! is_customize_preview() )
+    {
+       echo '<link rel="shortcut icon" type="image/png" href="'.get_template_directory_uri().'/assets/images/favicons/favicon.ico" />';
+    	
+		echo '<link rel="icon" type="image/png" sizes="16x16" href="'.get_template_directory_uri().'/assets/images/favicons/favicon-16x16.png" />';
+		echo '<link rel="icon" type="image/png" sizes="32x32" href="'.get_template_directory_uri().'/assets/images/favicons/favicon-32x32.png" />';
+		echo '<link rel="icon" type="image/png" sizes="96x96" href="'.get_template_directory_uri().'/assets/images/favicons/favicon-96x96.png" />';
+		echo '<link rel="apple-touch-icon" sizes="120x120" href="'.get_template_directory_uri().'/assets/images/favicons/apple-touch-icon-120x120.png" />';
+		echo '<link rel="apple-touch-icon" sizes="152x152" href="'.get_template_directory_uri().'/assets/images/favicons/apple-touch-icon-152x152.png" />';
+		echo '<link rel="apple-touch-icon" sizes="180x180" href="'.get_template_directory_uri().'/assets/images/favicons/apple-touch-icon-180x180.png" />';
+    }
+} 
--- header.php
+++ header.php
@@ -13,33 +13,10 @@
 		<meta class="foundation-mq">
 		<meta name="date" content="<?php the_modified_date(); ?>" />
 		<title><?php create_page_title(); ?></title>
-		<!-- If Site Icon isn't set in customizer -->
-		<?php if ( ! function_exists( 'has_site_icon' ) || ! has_site_icon() ) { ?>
-			<!-- Icons & Favicons -->
-	        <link rel="apple-touch-icon" sizes="57x57" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-57x57.png">
-		    <link rel="apple-touch-icon" sizes="60x60" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-60x60.png">
-		    <link rel="apple-touch-icon" sizes="72x72" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-72x72.png">
-		    <link rel="apple-touch-icon" sizes="76x76" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-76x76.png">
-		    <link rel="apple-touch-icon" sizes="114x114" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-114x114.png">
-		    <link rel="apple-touch-icon" sizes="120x120" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-120x120.png">
-		    <link rel="apple-touch-icon" sizes="144x144" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-144x144.png">
-		    <link rel="apple-touch-icon" sizes="152x152" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-152x152.png">
-		    <link rel="apple-touch-icon" sizes="180x180" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-180x180.png">
-		    <link rel="icon" type="image/png" sizes="192x192"  href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/android-icon-192x192.png">
-		    <link rel="icon" type="image/png" sizes="32x32" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/favicon-32x32.png">
-		    <link rel="icon" type="image/png" sizes="96x96" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/favicon-96x96.png">
-		    <link rel="icon" type="image/png" sizes="16x16" href="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/favicon-16x16.png">
-
-			<!--[if IE]>
-				<link rel="shortcut icon" href="<?php echo get_template_directory_uri(); ?>/favicon.ico">
-			<![endif]-->
-			<meta name="msapplication-TileColor" content="#f01d4f">
-			<meta name="msapplication-TileImage" content="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/apple-icon-120x120.png">
-	    	<meta name="theme-color" content="#121212">
-	    <?php } ?>
-
 		<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
 
+		<meta name="msapplication-config" content="<?php echo get_template_directory_uri(); ?>/assets/images/favicons/browserconfig.xml" />
+		
 		<?php wp_head(); ?>
 
 		<!-- Drop Google Analytics here -->
