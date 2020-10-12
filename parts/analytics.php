e77f2ece6c57dcb9cabea2493c8b8dbbdb08dab4
ksascomm <ksascomm@gmail.com>
2020-10-12 15:05:11 -0400

    Update to gtag.js

--- parts/analytics.php
+++ parts/analytics.php
@@ -1,15 +1,14 @@
 <?php $theme_option = flagship_sub_get_global_options();
       $analytics_id = $theme_option['flagship_sub_google_analytics']; ?>
 
+<!-- Global site tag (gtag.js) - Google Analytics -->
+<script async src="https://www.googletagmanager.com/gtag/js?id=UA-40512757-1"></script>
 <script>
-  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+	window.dataLayer = window.dataLayer || [];
+	function gtag(){dataLayer.push(arguments);}
+	gtag('js', new Date());
 
-  ga('create', '<?php echo $analytics_id; ?>', 'jhu.edu');
-  ga('create', 'UA-40512757-1', {'name': 'globalKSAS'});
-  ga('send', 'pageview');
-  ga('globalKSAS.send', 'pageview');
-
+	gtag('config', 'UA-40512757-1');
+	gtag('config', '<?php echo $analytics_id; ?>');
 </script>
+<!-- End Google Analytics -->
