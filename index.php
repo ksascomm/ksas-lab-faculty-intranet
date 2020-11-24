d9680f55511457489f822772ee57fcd437ad452b
ksascomm <ksascomm@gmail.com>
2020-11-24 12:42:15 -0500

    Fix featured image bug

--- index.php
+++ index.php
@@ -20,21 +20,7 @@ $theme_option = flagship_sub_get_global_options();
 
 		    <main class="main auto cell" role="main">
 
-				<?php
-				// If a featured image is set, insert into layout and use Interchange
-				// to select the optimal image size per named media query.
-				if ( has_post_thumbnail( $post->ID ) ) :
-			    ?>
-					<header class="featured-hero" role="banner" data-interchange="[<?php echo the_post_thumbnail_url('featured-small'); ?>, small], [<?php echo the_post_thumbnail_url('featured-medium'); ?>, medium], [<?php echo the_post_thumbnail_url('featured-large'); ?>, large], [<?php echo the_post_thumbnail_url('featured-xlarge'); ?>, xlarge]">
-					</header>
-
-					<h1 class="entry-title clear"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
-				
-				<?php else : ?>	
-
-					<h1 class="page-title"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
-					
-				<?php endif; ?>
+				<h1 class="page-title"><?php echo $theme_option['flagship_sub_feed_name']; ?> Archive</h1>
 
 			    <?php if (have_posts() ) : while (have_posts() ) : the_post(); ?>
 						<div class="news-feed">
