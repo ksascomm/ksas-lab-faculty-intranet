20920e87e819f36f588236d7c81729c752060e57
ksascomm <ksascomm@gmail.com>
2020-11-30 15:44:03 -0500

    logo and site title sizing fix

--- parts/nav-offcanvas-topbar.php
+++ parts/nav-offcanvas-topbar.php
@@ -13,46 +13,27 @@
 </div>
 
 <div class="top-bar hide-for-print" id="top-bar-menu">
-	
-		<div class="show-for-medium">
-			<div class="grid-x">
-				<div class="nav-shield">
-					<a href="http://krieger.jhu.edu">
-					<?php $theme_option = flagship_sub_get_global_options();
-					$color_scheme = $theme_option['flagship_sub_color_scheme'];
-					if ('spirit' === $color_scheme ) : ?>
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-black.png" class="float-center" alt="Krieger School of Arts and Sciences">
-					<?php else: ?>
-						<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-md.png" alt="Krieger School of Arts and Sciences">
-					<?php endif; ?>	
-					</a>
-				</div>
-				<div class="site-title">
-					<h1>
-						<a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'title' ); ?>
-							<?php if ( get_bloginfo( 'description' ) ) : ?>
-								<br>
-								<small><?php echo get_bloginfo( 'description' ); ?></small>
-							<?php endif; ?>
-						</a>
-					</h1>
-				</div>
-			</div>
+		<div class="nav-shield">
+			<a href="http://krieger.jhu.edu">
+			<?php $theme_option = flagship_sub_get_global_options();
+			$color_scheme = $theme_option['flagship_sub_color_scheme'];
+			if ('spirit' === $color_scheme ) : ?>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-black.png" class="float-center" alt="Krieger School of Arts and Sciences">
+			<?php else: ?>
+				<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-md.png" alt="Krieger School of Arts and Sciences">
+			<?php endif; ?>	
+			</a>
 		</div>
-		<ul class="vertical menu show-for-small-only">
-			<li>
-
-				<?php if ('spirit' === $color_scheme ) : ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-black.png" class="float-center" alt="Krieger School of Arts and Sciences">
-				<?php else: ?>
-					<img src="<?php echo get_template_directory_uri(); ?>/assets/images/ksas-horizontal-md.png" alt="Krieger School of Arts and Sciences">
-				<?php endif; ?>	
-
-			</li>
-			<li><h1 class="center"><a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'title' ); ?><br>
-				<small><?php echo get_bloginfo( 'description' ); ?></small></a></h1>
-			</li>
-		</ul>
+		<div class="site-title">
+			<h1>
+				<a href="<?php echo site_url(); ?>"><?php echo get_bloginfo( 'title' ); ?>
+					<?php if ( get_bloginfo( 'description' ) ) : ?>
+						<br>
+						<small><?php echo get_bloginfo( 'description' ); ?></small>
+					<?php endif; ?>
+				</a>
+			</h1>
+		</div>
 	<div class="top-bar-right float-right show-for-small-only">
 		<ul class="menu">
 			<li><button class="menu-icon" type="button" data-toggle="off-canvas"></button></li>
