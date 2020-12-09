30c3c8e7d38af3060494beaa585e1c02f80616de
ksascomm <ksascomm@gmail.com>
2020-12-09 14:27:51 -0500

    rewrite js defer function

--- functions/enqueue-scripts.php
+++ functions/enqueue-scripts.php
@@ -25,13 +25,10 @@ add_action('wp_enqueue_scripts', 'site_scripts', 999);
 
 // Defer non-essential/plugin javascript files
 // Defer jQuery Parsing using the HTML5 defer property
-if (!(is_admin() )) {
-    function defer_parsing_of_js ( $url ) {
-        if ( FALSE === strpos( $url, '.js' ) ) return $url;
-        if ( strpos( $url, 'jquery.min.js' ) ) return $url;
-        if ( strpos( $url, 'scripts.js' ) ) return $url;
-        // return "$url' defer ";
-        return "$url' defer onload='";
-    }
-    add_filter( 'clean_url', 'defer_parsing_of_js', 11, 1 );
+function defer_parsing_of_js( $url ) {
+	if ( is_user_logged_in() ) return $url; //don't break WP Admin
+	if ( FALSE === strpos( $url, '.js' ) ) return $url;
+	if ( strpos( $url, 'jquery.js' ) ) return $url;
+	return str_replace( ' src', ' defer src', $url );
 }
+add_filter( 'script_loader_tag', 'defer_parsing_of_js', 10 );
